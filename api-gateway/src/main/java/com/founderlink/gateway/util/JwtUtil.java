e21ad040290d129ea5cdf496255d04ae3d631a6a
VaibhavSaluja10 <abcxyz19691969@gmail.com>
2026-03-24 16:36:56 +0530

    Update JwtUtil.java

--- api-gateway/src/main/java/com/founderlink/gateway/util/JwtUtil.java
+++ api-gateway/src/main/java/com/founderlink/gateway/util/JwtUtil.java
@@ -12,7 +12,8 @@ import java.security.Key;
 @Component
 public class JwtUtil {
 
-    public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
+    @Value("${jwt.secret}")
+    private String SECRET;
 
     public void validateToken(final String token) {
         Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
